cbc0fc100468a9529671863a5aa530980e4a6305
Kubo Developer 7 <kubodeveloper7@outlook.com>
2022-12-21 16:51:32 -0600

    Cambios

--- paquetes/bin/paquetes.dart
+++ paquetes/bin/paquetes.dart
@@ -1,5 +1,21 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:paquetes/clases/reqres_respuesta.dart';
 import 'package:paquetes/paquetes.dart' as paquetes;
 
 void main(List<String> arguments) {
-  print('Hello world: ${paquetes.calculate()}!');
+  final url = Uri.parse('https://reqres.in/api/users?page=2');
+  http.get(url).then((res){
+    // print(value);
+    // final body = jsonDecode(res.body);
+    // print(body);
+    // print('page: ${body['page']}');
+    // print('per_page: ${body['per_page']}');
+    // print('id del tercer elemento: ${body['data'][2]['id']}');
+    final resReqRes = reqResRespuestaFromJson(res.body);
+    print('page: ${resReqRes.page}');
+    print('per_page: ${resReqRes.perPage}');
+    print('id del tercer elemento: ${resReqRes.data[2].id}');
+  });
+
 }
